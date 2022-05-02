a5d399abecd44fd75ed41d350480c1a5ceb90450
calebmct <jmctwigan@gmail.com>
2022-05-02 11:33:05 -0400

    updates
    contact page with PHP
    updated bio page

--- bio/scripts/bio.js
+++ bio/scripts/bio.js
@@ -1,7 +1,18 @@
+let maths = (420*Math.pow(.82,4));
 $(document).ready(function() {
-    let maths = (500*Math.pow(.84,4));
-    $(document).ready(function(){$("#bio_bg").css({'height':(($("#bio_blurb").height())+(maths)+'px')})});
+    $("#bio_bg").css({'height':(($("#bio_blurb").height())+(maths)+'px')});
+    var bioBg = $("#bio_bg").css({'height':(($("#bio_blurb").height())+(maths)+'px')});
+    $("#resume_section").css({'margin-top':(($("#bio_blurb").height())+(maths)+'px')});
+    console.log(bioBg);
+    animateBioBg();
+    console.log(2);
     $(window).resize(function() {
         $("#bio_bg").css({'height':(($("#bio_blurb").height())+(maths)+'px')});
-    })
-}); // ready function end
+        $("#resume_section").css({'margin-top':(($("#bio_blurb").height())+(maths)+'px')});
+        console.log("resize");
+    });
+});
+function animateBioBg() {
+    $("#bio_bg").css({'animation': 'bio_bg forwards 1s .5s ease-out'});
+    console.log(3);
+};
---
+++ contact/contact.php
@@ -0,0 +1,74 @@
+<?php
+if (isset($_POST['Email'])) {
+
+    $email_to = "jcaleb.mctwigan@gmail.com";
+    $email_subject = "Portfolio Site - Contact Submission";
+
+    function problem($error)
+    {
+        echo "Whoops there were error(s) found with the form you submitted:<br><br>";
+        echo $error . "<br><br>";
+        echo "Please try fixing the errors to submit your information.<br><br>";
+        die();
+    }
+
+    // validation expected data exists
+    if (
+        !isset($_POST['name']) ||
+        !isset($_POST['email']) ||
+        !isset($_POST['message'])
+    ) {
+        problem('Sorry, but there appears to be a problem with the form you submitted.');
+    }
+
+    $name = $_POST['name']; // required
+    $email = $_POST['email']; // required
+    $message = $_POST['message']; // required
+
+    $error_message = "";
+    $email_exp = "/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/";
+
+    if (!preg_match($email_exp, $email)) {
+        $error_message .= 'The bots are not happy with this email address.<br>';
+    }
+
+    $string_exp = "/^[A-Za-z .'-]+$/";
+
+    if (!preg_match($string_exp, $name)) {
+        $error_message .= 'Hmmm, try that with less special characters, maybe?<br>';
+    }
+
+    if (strlen($message) < 2) {
+        $error_message .= 'Please elongate your message.<br>';
+    }
+
+    if (strlen($error_message) > 0) {
+        problem($error_message);
+    }
+
+    $email_message = "Form details below.\n\n";
+
+    function clean_string($string)
+    {
+        $bad = array("content-type", "bcc:", "to:", "cc:", "href");
+        return str_replace($bad, "", $string);
+    }
+
+    $email_message .= "Name: " . clean_string($name) . "\n";
+    $email_message .= "Email: " . clean_string($email) . "\n";
+    $email_message .= "Message: " . clean_string($message) . "\n";
+
+    // create email headers
+    $headers = 'From: ' . $email . "\r\n" .
+        'Reply-To: ' . $email . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+    @mail($email_to, $email_subject, $email_message, $headers);
+?>
+
+    <!-- INCLUDE YOUR SUCCESS MESSAGE BELOW -->
+
+    Thanks for getting in touch!
+
+<?php
+}
+?>
