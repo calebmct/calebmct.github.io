fb04e6e98632a1da9e7f28403c98fd799ef07ff5
calebmct <jmctwigan@gmail.com>
2023-04-25 16:33:00 -0400

    file location update (js)

--- portfolio/scripts/portfolio.js
+++ portfolio/scripts/portfolio.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var imgurl = 'images/CM-logo.gif'+'?a='+Math.random();
+    var imgurl = '../images/CM-logo.gif'+'?a='+Math.random();
     $("#logo img").attr('src',imgurl);
     $("#logo img").attr('alt','Caleb McTwigan');
     console.log(imgurl);
