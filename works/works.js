e56796cded1ad187b648c5143af7d3ffde2b70db
calebmct <jmctwigan@gmail.com>
2023-10-31 17:02:22 -0400

    Formatting Updates and Graphic Design Samples added
    Added all graphic design samples
    Background formatting
    Sticky headers
    and more!

--- works/works.js
+++ works/works.js
@@ -1,2 +1,22 @@
 $(document).ready(function() {
-}); // ready function end
+
+    Array.from(document.querySelectorAll('.lightbox')).forEach(element => {
+        element.addEventListener('click', clickEvent => {
+            activateLightbox(element.id);
+        });
+    })
+        
+    function activateLightbox(id) {
+        $("#"+id+"-full")
+        .removeClass("lightbox-target-inactive")
+        .addClass("lightbox-target-active");
+        console.log("#"+id+"-full");
+    };
+
+    $('.lightbox-close').on('click', function() {
+        $('.lightbox-target')
+        .removeClass('lightbox-target-active')
+        .addClass('lightbox-target-inactive');
+    });
+});
+// ready function end
